2b364ebf6e712e2a23683d5d7b9b90abd175b2fe
Aluno.Etec <aula@etec.com>
2025-08-04 22:25:54 -0300

    Primeira aula de php

---
+++ PHP/aula.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    
+    <title>Document</title>
+</head>
+<body>
+        <?php
+            $a = 5;
+            $b = 7;
+            $c = 9;
+            if ($a < 6){
+                  echo "Parabens voce conseguiu";
+            }
+             if ($b < 6){
+                  echo "Não é o menor numero ;-;";
+            } 
+             if ($c < 6){
+                  echo "Não é o menor numero ;-;";
+            }  
+        ?>
+</body>
+</html>
